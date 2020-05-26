d799d4b2d21a8d7f28181a3af7c2ed4c4047fa6b
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-05-26 18:06:15 -0400

    Finished implementing zoom when user presses the locatr button

--- app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
+++ app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -17,9 +17,16 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,6 +43,10 @@ public class LocatrFragment extends SupportMapFragment {
     private Bitmap mMapImage;
     private GalleryItem mMapItem;
     private Location mCurrentLocation;
+
+    public static LocatrFragment newInstance() {
+        return new LocatrFragment();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -60,6 +71,7 @@ public class LocatrFragment extends SupportMapFragment {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
+                updateUI();
             }
         });
     }
@@ -135,6 +147,36 @@ public class LocatrFragment extends SupportMapFragment {
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
+    private void updateUI() {
+        if(mMap == null || mMapImage == null) {
+            return;
+        }
+
+        LatLng itemPoint = new LatLng(mMapItem.getLat(), mMapItem.getLon());
+        LatLng myPoint = new LatLng(
+                mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+        BitmapDescriptor itemBitmap = BitmapDescriptorFactory.fromBitmap(mMapImage);
+        MarkerOptions itemMarker = new MarkerOptions()
+                .position(itemPoint)
+                .icon(itemBitmap);
+        MarkerOptions myMarker = new MarkerOptions()
+                .position(myPoint);
+
+        mMap.clear();
+        mMap.addMarker(itemMarker);
+        mMap.addMarker(myMarker);
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(itemPoint)
+                .include(myPoint)
+                .build();
+
+        int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
+        CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, margin);
+        mMap.animateCamera(update);
+    }
+
     private class SearchTask extends AsyncTask<Location, Void, Void> {
         private GalleryItem mGalleryItem;
         private Bitmap mBitmap;
@@ -165,6 +207,7 @@ public class LocatrFragment extends SupportMapFragment {
             mMapItem = mGalleryItem;
             mMapImage = mBitmap;
             mCurrentLocation = mLocation;
+            updateUI();
         }
     }
 }
